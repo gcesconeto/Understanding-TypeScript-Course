23e47f61553b5933223abf27c7284a51964b08ee
Guilherme Cesconeto <cesconeto@gmail.com>
2022-08-25 14:25:11 -0300

    05: optional props

--- 05-classesInterfaces/interfaces.ts
+++ 05-classesInterfaces/interfaces.ts
@@ -1,6 +1,9 @@
-interface Greetable {  // not the same as custom type, works only for objects
-  name: string;
+interface Named { // not the same as custom type, works only for objects
+  readonly name: string; // makes sure that whatever implements this, cant reassign this prop
+  outputName?: string; // optional prop, also works on classes
+}
 
+interface Greetable extends Named{  // extend interfaces to inherit "rules" from parent (an extend multiple)
   greet(message: string): void;
 }
 
@@ -14,7 +17,7 @@ class Person implements Greetable {
   }
 
   greet(message: string) {
-    console.log(message + this.name)
+    console.log(message + this.name);
   }
 }
 
@@ -23,3 +26,21 @@ let user1: Greetable; // As Person class implements Greetable, user 1 can be an 
 user1 = new Person('Naty', 30); 
 
 user1.greet('Hello, my name is ');
+
+console.log(user1);
+
+// Interface as Fn Types:
+
+// type AddFn = (n1: number, n2: number) => number;
+
+interface AddFn {
+  (n1: number, n2: number): number;
+}
+
+let add: AddFn;
+
+add = (a: number, b: number) => {
+ return a + b;
+}
+
+export {}
