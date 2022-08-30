48187c8ef3d3a00d18d839c73f0ba5895837c9e5
Guilherme Cesconeto <cesconeto@gmail.com>
2022-08-30 15:05:40 -0300

    08: useful decorators

--- 08-decorators/decorators.ts
+++ 08-decorators/decorators.ts
@@ -36,3 +36,17 @@ class Person2 {
   }
 };
 
+// Useful Decorators
+
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent += p.name
+    }
+  }
+}
+
+// @WithTemplate('<h1>Inner HTML replacer</h1>', 'app') // does more advanced things
