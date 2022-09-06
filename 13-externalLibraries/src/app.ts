f9aac2ffd3230cb961b9b5acbe6dddf69414fd8e
Guilherme Cesconeto <cesconeto@gmail.com>
2022-09-06 13:56:17 -0300

    13: declare statement

--- 13-externalLibraries/src/app.ts
+++ 13-externalLibraries/src/app.ts
@@ -1,3 +1,7 @@
 import lodash from 'lodash';
 
+declare var GLOBAL: any; // tells TS that this variable exists no matter what, and prevents errors
+
 console.log(lodash.shuffle([1, 2, 3, 4, 5, 6]));
+
+console.log(GLOBAL);
