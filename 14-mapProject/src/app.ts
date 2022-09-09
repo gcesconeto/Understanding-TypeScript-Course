79d0f11decb5d91d4c85809194beb4a8215006b6
Guilherme Cesconeto <cesconeto@gmail.com>
2022-09-09 10:47:40 -0300

    15: base react setup

--- 14-mapProject/src/app.ts
+++ 14-mapProject/src/app.ts
@@ -1,39 +1,39 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+// const form = document.querySelector('form')!;
+// const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-type GeocodingResponse = {
-  results: {geometry: { location: {lat: number; lng: number}}}[];
-  status: 'OK' | 'ZERO_RESULTS';
-};
+// type GeocodingResponse = {
+//   results: {geometry: { location: {lat: number; lng: number}}}[];
+//   status: 'OK' | 'ZERO_RESULTS';
+// };
 
-async function searchAddressHandler(event: Event) {
-  event.preventDefault();
-  const searchInput = addressInput.value;
-  try {
-    const response = await axios.get<GeocodingResponse>(
-      `${BASE_URL}${encodeURI(searchInput)}&key=${KEY}`
-    );
+// async function searchAddressHandler(event: Event) {
+//   event.preventDefault();
+//   const searchInput = addressInput.value;
+//   try {
+//     const response = await axios.get<GeocodingResponse>(
+//       `${BASE_URL}${encodeURI(searchInput)}&key=${KEY}`
+//     );
 
-    if (response.data.status !== 'OK') throw new Error('Request error')
+//     if (response.data.status !== 'OK') throw new Error('Request error')
 
-    const coordinates = response.data?.results[0]?.geometry?.location;
+//     const coordinates = response.data?.results[0]?.geometry?.location;
 
-    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-      center: coordinates,
-      zoom: 16,
-    });
+//     const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+//       center: coordinates,
+//       zoom: 16,
+//     });
 
-    new google.maps.Marker({ position: coordinates, map: map })
+//     new google.maps.Marker({ position: coordinates, map: map })
 
-    console.log(map);
+//     console.log(map);
 
-  } catch (error) {
-    alert(error);
-    console.log(error);
-  }
+//   } catch (error) {
+//     alert(error);
+//     console.log(error);
+//   }
 
-}
+// }
 
-form.addEventListener('submit', searchAddressHandler)
+// form.addEventListener('submit', searchAddressHandler)
