80c33bc763f4242f880a701ebf6245dd4bd89389
Guilherme Cesconeto <cesconeto@gmail.com>
2022-09-09 10:30:09 -0300

    14: finished, but without keys

--- 14-mapProject/src/app.ts
+++ 14-mapProject/src/app.ts
@@ -1,1 +1,39 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+type GeocodingResponse = {
+  results: {geometry: { location: {lat: number; lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+async function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const searchInput = addressInput.value;
+  try {
+    const response = await axios.get<GeocodingResponse>(
+      `${BASE_URL}${encodeURI(searchInput)}&key=${KEY}`
+    );
+
+    if (response.data.status !== 'OK') throw new Error('Request error')
+
+    const coordinates = response.data?.results[0]?.geometry?.location;
+
+    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: coordinates,
+      zoom: 16,
+    });
+
+    new google.maps.Marker({ position: coordinates, map: map })
+
+    console.log(map);
+
+  } catch (error) {
+    alert(error);
+    console.log(error);
+  }
+
+}
+
+form.addEventListener('submit', searchAddressHandler)
