5b78b3c02ddf0690426be76fee0128bac9141e66
Guilherme Cesconeto <cesconeto@gmail.com>
2022-09-10 18:27:25 -0300

    16: refactor

--- 16-expressJS/src/constrollers/todos.ts
+++ 16-expressJS/src/constrollers/todos.ts
@@ -3,10 +3,12 @@ import { RequestHandler } from "express";
 import { Todo } from "../models/todo";
 
 const TODOS: Todo[] = [];
+let id = 1;
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const todoText = (req.body as { text: string }).text;
-  const newTodo = new Todo(Math.random().toString(), todoText);
+  const newTodo = new Todo(id.toString(), todoText);
+  id += 1;
 
   TODOS.push(newTodo);
 
@@ -17,21 +19,25 @@ export const listTodos: RequestHandler = (req, res, next) => {
   res.status(200).json({ message: 'todo list', list: TODOS })
 }
 
-export const editTodo: RequestHandler = (req, res, next) => {
-  const todoId = req.params.id as string;
-  const editTodo = TODOS.find(({ id }) => todoId === id);
-  if (!editTodo) return next('Todo not found');
+export const editTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const editIndex = TODOS.findIndex(({ id }) => todoId === id);
+
+  if (editIndex === -1) return next('Todo not found');
+
   const todoText = (req.body as { text: string }).text;
-  TODOS[TODOS.indexOf(editTodo)].text = todoText;
+  TODOS[editIndex].text = todoText;
 
   res.status(201).json({ message: 'todo edited', edited: todoText })
 }
 
-export const deleteTodo: RequestHandler = (req, res, next) => {
-  const todoId = req.params.id as string;
-  const deleteTodo = TODOS.find(({ id }) => todoId === id);
-  if (!deleteTodo) return next('Todo not found');
-  TODOS.splice(TODOS.indexOf(deleteTodo), 1);
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const deleteIndex = TODOS.findIndex(({ id }) => todoId === id);
+
+  if (deleteIndex === -1) return next('Todo not found');
+  
+  TODOS.splice(deleteIndex, 1);
 
   res.status(200).json({ message: 'todo deleted' })
 }
